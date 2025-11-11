4e4ce5b09bab9a14af27ebc514b91a02d05c77e5
Sergei Ubozhenko <ubozhenko.sergey@gmail.com>
2025-11-11 14:59:06 +0300

    add queue into tb

--- lab0/task3_delay_15/tb/delay_15_top_tb.sv
+++ lab0/task3_delay_15/tb/delay_15_top_tb.sv
@@ -1,45 +1,76 @@
-`timescale 1 ps / 1 ps
+`timescale 1ns / 1ps
+
 module delay_15_top_tb;
-  logic       data_input;
-  logic [3:0] data_delay_input;
-  logic       data_output;
+  localparam WIDTH = 8;
 
-  bit         clock_input;
-  bit         rst_input;
+  logic       clk = 0;
+  logic       rst = 1;
+  logic       din;
+  logic [3:0] delay;
+  logic       dout;
 
-  bit         rst_done;
+  bit         rst_done = 0;
+
+  typedef struct {
+    logic [WIDTH-1:0] value;
+    int               delay_cycles;
+  } input_info_t;
+
+  input_info_t inputs_q[$];
+
+  logic        outputs [$];
 
   delay_15_top DUT (
-    .data_i      (data_input),
-    .clk_i       (clock_input),
-    .rst_i       (rst_input),
-    .data_delay_i(data_delay_input),
-    .data_o      (data_output)
+    .clk_i       (clk),
+    .rst_i       (rst),
+    .data_i      (din),
+    .data_delay_i(delay),
+    .data_o      (dout)
   );
 
+  initial forever #5 clk = !clk;
+
+  default clocking cb @(posedge clk);
+  endclocking
+
   initial begin
-    rst_input <= 1'b1;
-    #1;
-    rst_input <= 1'b0;
-    #1;
-    rst_input <= 1'b1;
-    rst_done = 1'b1;
+    rst = 0;
+    ##1;
+    rst      = 1;
+    rst_done = 1;
   end
 
-  initial forever #1 clock_input = !clock_input;
-
-  logic [3:0] data_pattern = 4'b1010;
-
+int rand_val;
+int rand_val_before;
+logic rand_val_q [$];
+  // Stimulus
   initial begin
-    data_delay_input = {4'b1110};
+    $display("Starting test...");
     wait (rst_done);
-    #9;
-    for (int i = 0; i < 4; i++) begin
-      data_input = data_pattern[i];
-      #10;
+    delay = $urandom_range(0, 15);
+    rand_val = $urandom_range(0, 2 ** WIDTH - 1);
+    
+    ##2;
+    repeat (WIDTH + delay + 10) begin
+      din = rand_val % 2;
+      rand_val_q.push_back(din);
+      rand_val >>= 1;
+
+      // Push into tracking queue
+      outputs.push_back(dout);
+
+      @(posedge clk);
     end
-    data_input = 0;
     #50;
+    $display("Test finished.");
+    rand_val_q.reverse();
+    foreach (rand_val_q[i]) $write("%b", rand_val_q[i]);
+    $display();
+    outputs.reverse();
+    repeat(delay) outputs.pop_front();
+    
+    foreach (outputs[i]) $write("%b", outputs[i]);
     $stop;
   end
+
 endmodule
